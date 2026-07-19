1ca92664edb7e0ec5dffe99d635e3b6d9f140ba7
andrewkuma <ysman28m2q@web-library.net>
2026-07-19 02:40:18 +0000

    Cache static assets for 7 days in browser

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,19 @@ const nextConfig = {
       beforeFiles: [{ source: "/", destination: "/home.html" }],
     };
   },
+  async headers() {
+    return [
+      {
+        source: "/assets/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=604800, stale-while-revalidate=86400",
+          },
+        ],
+      },
+    ];
+  },
   transpilePackages: ["next-mdx-remote"],
   images: {
     remotePatterns: [
